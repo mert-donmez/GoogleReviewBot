f0ff665b11bba045de0e766cecfc38723893f3e7
mert-donmez <83416622+mert-donmez@users.noreply.github.com>
2022-01-26 00:09:16 +0300

    Update GoogleReviewBot.py

--- GoogleReviewBot.py
+++ GoogleReviewBot.py
@@ -17,7 +17,7 @@ class GoogleReviewBot:
 
     def initialize(self):
         self.i = 0
-        PlaceURL = "https://www.google.com/search?q=empire+state+building&biw=1920&bih=841&tbm=lcl&sxsrf=AOaemvKtJq3ct8p65NL3BUMpvXtXXWJ2Ww%3A1641592752932&ei=sLfYYZmvONiRxc8PwLqiiA8&oq=empire+state&gs_l=psy-ab.1.1.35i39k1j0i433i67k1j0i512k1l7j0i67k1.63774.65241.0.67175.12.10.0.0.0.0.384.1764.0j1j5j1.7.0....0...1c.1.64.psy-ab..5.7.1764...0i512i433k1j0i512i433i131k1j0i512i457k1.0.-OO0zl_w5LU#lrd=0x89c259a9b3117469:0xd134e199a405a163,3,,,&rlfi=hd:;si:15074921902713971043,l,ChVlbXBpcmUgc3RhdGUgYnVpbGRpbmdIn6MFWicQAhgAGAEYAiIVZW1waXJlIHN0YXRlIGJ1aWxkaW5nKgIIAjICZW6SARJ0b3VyaXN0X2F0dHJhY3Rpb24,y,IYrXyp60Thk;mv:[[40.74862047731903,-73.98542683213199],[40.74826052268097,-73.98590196786799]]" #ENTER YOUR LINK HERE
+        PlaceURL = "--" #ENTER YOUR LINK HERE
         self.driver = uc.Chrome()
         self.driver.delete_all_cookies()
         self.urls = ["https://accounts.google.com/signin/v2/identifier?hl=tr&passive=true&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dgoogle%26oq%3Dgoogle%26aqs%3Dchrome.0.69i59l3j0i271l2j69i60j69i65j69i60.706j0j1%26sourceid%3Dchrome%26ie%3DUTF-8&ec=GAZAAQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin",PlaceURL]
